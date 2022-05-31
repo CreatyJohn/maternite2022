cc79a86c5dce16e907e77fb938469f9178b1d38c
Brave Kim <ygflove@bible.ac.kr>
2022-05-31 22:03:15 +0900

    :adhesive_bandage: Fix regex error
    and remove http library

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 // 이용 약관에 따르면 DELETE는 user정보를 지우기 위해 반드시 필요하다,
 const express = require("express");
 const app = express();
-const fs = require("fs");
 const path = require("path");
-const http = require("http");
 
 // PORT number
 const PORT = 3000;
 
 app.use("/private", express.static(path.join(__dirname, "private")));
-app.use(express.static(path.join(__dirname, "./public")));
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  fs.createReadStream("index.html").pipe(res);
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 // listen server
-server.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
